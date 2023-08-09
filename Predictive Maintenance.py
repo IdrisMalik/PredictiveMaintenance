deabda8561d052718665149e765bd76de5bf600f
IdrisMalik <89416208+IdrisMalik@users.noreply.github.com>
2023-08-09 14:53:50 +0530

    Predictive Maintenance.py

--- Predictive Maintenance.py
+++ Predictive Maintenance.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('predictive_maintenance.csv')
 X = data.drop('target_column', axis=1)
 y = data['target_column']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
