b57565e0c5c0b10872f63a3c0623ead15d430bc6
IdrisMalik <89416208+IdrisMalik@users.noreply.github.com>
2023-08-09 14:40:17 +0530

    Predictive Maintenance.py

--- Predictive Maintenance.py
+++ Predictive Maintenance.py
@@ -4,7 +4,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load and preprocess the dataset (replace 'data.csv' with your dataset file)
 data = pd.read_csv('data.csv')
 X = data.drop('target_column', axis=1)
 y = data['target_column']
@@ -13,18 +12,14 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Train a Random Forest Classifier (you can choose other algorithms too)
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train_scaled, y_train)
 
-# Make predictions
 y_pred = model.predict(X_test_scaled)
 
-# Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy:.2f}")
 
-# Deploy the model for real-time predictions and monitoring
 def predict_machine_condition(new_data):
     new_data_scaled = scaler.transform(new_data)
     prediction = model.predict(new_data_scaled)
